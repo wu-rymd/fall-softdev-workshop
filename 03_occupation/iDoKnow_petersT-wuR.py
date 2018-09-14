5a1bf74d6df57fb843fff2f0bdd2952001c5b8ba
raywu6 <rwu6@stuy.edu>
2018-09-13 21:55:25 -0400

    import random.choices(), add descriptive comments, add getOccupation() method

--- 03_occupation/iDoKnow_petersT-wuR.py
+++ 03_occupation/iDoKnow_petersT-wuR.py
@@ -3,4 +3,23 @@
 # K6 -- StI/O: Divine your Destiny!
 # 2018-09-14
 
+from random import choices
+# imports the 'choices' method from the 'random' module 
+# https://docs.python.org/3/library/random.html#random.choices
 
+# random.choices(population, weights=None ...)
+# Return a k sized list of elements chosen from the population with replacement.
+# If a weights sequence is specified, selections are made according to the relative weights.
+
+# TODO import csv module
+
+occupations = {}
+
+# TODO parse thru csv, populate occupations dict
+
+def getOccupation():
+    # list of occupation strings -- copied into popluation parameter of random.choices()
+    # list of weight floating points -- copied into the weights parameter of random.choices()
+    return random.choices(occupations.keys(), weights=occupations.values())
+
+# getOccupation()

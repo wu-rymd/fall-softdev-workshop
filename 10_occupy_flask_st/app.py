2269042ced67ccf97972f18cc8fdaf70f5c7f45b
raywu6 <rwu6@stuy.edu>
2018-09-23 01:07:01 -0400

    add K #10, vanilla version

---
+++ 10_occupy_flask_st/app.py
@@ -0,0 +1,42 @@
+# Team R (Rachel Ng, Raymond Wu)
+# SoftDev1 pd7
+# K10 -- Jinja Tuning
+# 2018-09-24
+
+from flask import Flask, render_template
+from csv import reader
+from random import choice
+
+app = Flask(__name__)  # create instance of class Flask
+
+# this function is not assigned to a route!
+# returns a dictionary with occupation data
+def csvToDict():
+    occupations = {}  # prepare dict for occupation data 
+    csvFileObject = open('./data/occupations.csv', 'r')  # open the csv file object in 'read' mode
+    readerObject = reader(csvFileObject)  # read the records in the csv
+
+    for record in readerObject:
+        if record[0] == "Job Class" or record[0] == "Total":  
+            continue  # skip the first and last records
+        
+        # record[0] is the occupation string, record[1] is the floating point (as a string)
+        # must convert the appropriate string into floating point before placing as value in dict
+        else:
+            occupations[ record[0] ] = float(record[1])
+            
+    csvFileObject.close()
+    return occupations
+
+# this function is assigned to a route...
+@app.route("/occupations")
+def showOccupations():
+    return render_template("occupationsTable.html",
+                           occupations = csvToDict(),  # `occupation` here refers to Jinja var
+                           randomOccupation = choice( list(csvToDict().keys()) ),
+                           # puts the keys to occupation dict in a list, choose randomly from that list
+    )
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()

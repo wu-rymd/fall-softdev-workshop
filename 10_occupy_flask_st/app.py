1757fe8eb4e6a39aa754497a810f845f8a2daf66
raywu6 <rwu6@stuy.edu>
2018-09-26 08:40:55 -0400

    rebase

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -4,59 +4,23 @@
 # 2018-09-25
 
 from flask import Flask, render_template
-from csv import reader
 from random import choices
+from util import parseData
 
 app = Flask(__name__)  # create instance of class Flask
 
-occupations = {}
-weights = []
 
-# POSTCONDITION: occupations dict is populated with occupation data
-def csvToDict():
-    csvFileObject = open('./data/occupations.csv', 'r')  # open the csv file object in 'read' mode
-    readerObject = reader(csvFileObject)  # read the records in the csv
-
-    for record in readerObject:
-        if record[0] == "Job Class" or record[0] == "Total":  
-            continue  # skip the first and last records
-        
-        # record[0] is occupation string, record[1] is floating point (as a string), record[2] is link
-        # must convert the appropriate string into floating point before placing as value in dict
-        # occupation : [ <percentage> , <link> ]
-        else:
-            occupations[ record[0] ] = [ float( record[1] ) , record[2] ]
-            
-    csvFileObject.close()
-
-#  PRECONDITION: occupations dict cannot be empty (csvToDict must have ran first)
-# POSTCONDITION: weights list is populated w/ weighted percentages of randomness
-def dictWeights():
-    listValues = list(occupations.values()) # list containing a list of the values in occupations dict
-    # the values themselves are lists of the form [ <percentage> , <link> ]
-
-    for listOfOccupationData in listValues:             # for each value in current list of occupation data
-        weights.append( listOfOccupationData[0] )       # put the percentage in list of weights
-
-# these accessor methods are necessary b/c
-# it doesn't make sense to keep reading the csv file & re-populate the occupations dict
-# every time we want to access the data
-#
-# POSTCONDITION (both functions): occupations{} and weights[] must be populated w/ data
-def getOccupationsDict():
-    return occupations
-
-def getWeights():
-    return weights
-
+@app.route("/")
+def home():
+    return "<a href='occupations'>Click here for occupations...</a>"
 
 # this function is assigned to a route...!
 @app.route("/occupations")
 def showOccupations():
 
     return render_template("occupationsTable.html",
-                           occupations = getOccupationsDict(),  # `occupation` here refers to Jinja var
-                           randomOccupation = choices( list(getOccupationsDict().keys()) , weights = getWeights() )[0]
+                           occupations = parseData.getOccupationsDict(),  # `occupation` here refers to Jinja var
+                           randomOccupation = choices( list(parseData.getOccupationsDict().keys()) , weights = parseData.getWeights() )[0]
                            # puts the keys to occupation dict in a list, choose randomly (weighted) from that list
                            # random.choices() returns a k-sized list (default=1) ... only concerned with first output
     )
@@ -66,8 +30,8 @@ if __name__ == "__main__":
     # must populate dictionary & list with data
     # these methods are not linked to a Flask route,
     # so not necessary that app is running...
-    csvToDict()
-    dictWeights()
+    parseData.csvToDict()
+    parseData.dictWeights()
     
     app.debug = True
     app.run()

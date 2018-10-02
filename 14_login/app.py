45ec85cbe46d22434be2cbf04a36b53d9ac53ef0
raywu6 <rwu6@stuy.edu>
2018-10-01 22:25:54 -0400

    merge work from temp K #14 collab repo

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,27 +1,62 @@
-from flask import Flask, render_template, session
+# Team IDoKnowYou -- Daniel Keriazis, Raymond Wu
+# SoftDev pd7
+# K14 -- Do I Know You?
+# 2018-10-01
+
+from flask import Flask, redirect, render_template, request, session, url_for
 from util import tools
 
 app = Flask(__name__)
-
 app.secret_key = tools.generate_random_secretkey()
 
-@app.route("/", methods=["GET", "POST"])
+credentials = {'user':'pass'} # hardcoded user/pass combo
+
+@app.route("/")
 def homepage():
-    return render_template("index.html", username=username)
+    # if already logged in
+    if 'username' in session:
+        return render_template("index.html", username=session['username'])
+    else:
+        return redirect('/login')
 
-@app.route("/auth", methods=["POST"])
-def auth():
-    return ""
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+
+        # if already logged in
+        if 'username' in session:
+            return redirect( '/' )
+
+        # reach here: not logged in
+        return render_template("login.html")
+
+    # reach here: request.method = "POST"
+
+    # get values passed via POST
+    username = request.form.get("user")
+    password = request.form.get("pass")
+
+    # username is not a key in the dictionary of all users
+    if username not in credentials.keys():
+        return "Username not found! <a href='login'>Click to go back.</a>"
+    # username exists, but password incorrect
+    elif credentials[username] != password:
+        return "Wrong password! <a href='login'>Click to go back.</a>"
+    # username exists, password correct
+    else:
+        session['username'] = username
+        return redirect('/')
 
 @app.route("/register")
 def register():
     return render_template("register.html")
 
+@app.route("/logout")
+def logout():
+    session.pop('username')
+    return redirect('/login')
+
 if __name__ == "__main__":
-    app.debug = True  # set to False in production mode!
+    app.debug = False  # set to False in production mode!
     app.run()
 

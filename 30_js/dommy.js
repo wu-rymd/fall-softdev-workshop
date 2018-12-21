864dc81d0ce906a3e5b63fda1670085c276bd496
raywu6 <rwu6@stuy.edu>
2018-12-21 13:09:48 -0500

    add K30 phase IV

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -1,7 +1,12 @@
-// HEADING HERE
+// Peter Cwalina, Raymond Wu (team PCRW)
+// SoftDev1 pd7
+// K30 -- Sequential Progression III: Season of the Witch
+// 2018-12-21
 
-var listButton = document.getElementById('b');
+// PHASE III
+
 var orderedList = document.getElementById('thelist');
+var listButton = document.getElementById('b');
 
 listButton.addEventListener('click', function() {
     // create new list item
@@ -10,23 +15,22 @@ listButton.addEventListener('click', function() {
 
     // append new list item to ordered list
     orderedList.appendChild(newListItem);
-    
 }
                            );
 
-var headingTag = document.getElementById('h');
+var headingElem = document.getElementById('h');
 
 // show list item on mouseover
 orderedList.addEventListener('mouseover', function(e) {
-    console.log(e);
+    // console.log(e);
     let itemText = e['path'][0]['innerHTML'];
-    headingTag.innerHTML = itemText;
+    headingElem.innerHTML = itemText;
 }
                             );
 
 // revert heading to Hello World! if mouse outside <ol>
 orderedList.addEventListener('mouseout', function() {
-    headingTag.innerHTML = 'Hello World!';
+    headingElem.innerHTML = 'Hello World!';
 }
                             );
 
@@ -36,3 +40,24 @@ orderedList.addEventListener('click', function(e) {
     listItemElem.remove();
 }
                             );
+
+// PHASE IV
+
+var fibList = document.getElementById('fiblist');
+var fibButton = document.getElementById('fb');
+
+var fibArray = [0, 0, 1];
+fibButton.addEventListener('click', function() {
+    fibArray[0] = fibArray[1];
+    fibArray[1] = fibArray[2];
+    fibArray[2] = fibArray[0] + fibArray[1];
+
+    // create new list item
+    let newFibItem = document.createElement('li');
+    newFibItem.innerHTML = fibArray[1];
+    
+    // append new list item to ordered list
+    fibList.appendChild(newFibItem);
+}
+                          );
+
